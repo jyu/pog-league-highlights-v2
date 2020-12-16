034ffb589ed5ca6f102eacf48a45115a1e1ea884
Jerry Yu <jerryyu8@gmail.com>
2020-12-15 23:30:32 -0500

    Add multi and solo kills

--- highlights.py
+++ highlights.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import datetime
 import time
 from secret import get_api_key
+from champions import get_champions_name
 
 watcher = LolWatcher(get_api_key())
 region = 'na1'
@@ -35,7 +36,7 @@ for match in matches['matches']:
   # Only want on role games
   if match['lane'] != LANE or match['role'] != ROLE:
     continue
-
+  
   # League timestamp is in ms
   # timestamp = match['timestamp'] / 1000
   # if timestamp < month_start or timestamp > month_end:
@@ -44,33 +45,62 @@ for match in matches['matches']:
 
   res.append(match)
 
-for match in res:
-  print(match)
-
+for match in res[:10]:
   match_info = watcher.match.by_id(region, match['gameId'])
+
+  game_end = match_info["gameDuration"]
+  game_type = match_info["gameType"]
+  game_version = match_info["gameVersion"]
+  queue_id = match_info["queueId"]
 
   # Get the participant ID fro match info
   participant_id = None
   for participant in match_info['participantIdentities']:
     if participant['player']['accountId'] == user['accountId']:
       participant_id = participant['participantId']
-      # print(participant)
+
+  is_multikill = False
+  highlight_times = []
 
   # For match participant info
-  # for participant in match_info['participants']:
-  #   if participant['participantId'] == participant_id:
-  #     print(participant['stats']['tripleKills'])
-  #     print(participant['stats']['quadraKills'])
-  #     print(participant['stats']['pentaKills'])
+  for participant in match_info['participants']:
+    if participant['participantId'] == participant_id:
+      triple = participant['stats']['tripleKills']
+      quadra = participant['stats']['quadraKills']
+      penta = participant['stats']['pentaKills']
+      if triple > 0 or quadra > 0 or penta > 0:
+        is_multikill = True
 
+  is_highlight = is_multikill
   timeline = watcher.match.timeline_by_match(region, match['gameId'])
   # 1 frame per min
-  print(timeline.keys())
-  print(len(timeline['frames']))
-  print(timeline['frameInterval'])
+  all_kill_times = []
   for frame in timeline['frames']:
     for event in frame['events']:
       if event['type'] == 'CHAMPION_KILL' and event['killerId'] == participant_id:
-        print(event)
-    
-  break
+
+        if len(event['assistingParticipantIds']) == 0:
+          highlight_times.append({'time': event['timestamp'] / (1000 * 60), 'info': 'solo_kill'})
+          is_highlight = True
+
+        all_kill_times.append(event['timestamp'])
+
+  if is_multikill:
+    kt_diff = []
+    for i in range(len(all_kill_times)):
+      if i == 0:
+        kt_diff.append(0)
+      else:
+        kt_diff.append((all_kill_times[i] - all_kill_times[i-1]) / 1000)
+
+    for i in range(len(kt_diff)):
+      if i == 0:
+        continue
+      if kt_diff[i] < 10:
+        highlight_times.append({'time': all_kill_times[i-1] /(1000 * 60), 'info': 'multikill'})
+
+  if is_highlight and len(highlight_times) > 0:
+    print("game_id", match['gameId'])
+    print("champion", get_champions_name(match['champion']))
+    print(highlight_times)
+  time.sleep(1)
